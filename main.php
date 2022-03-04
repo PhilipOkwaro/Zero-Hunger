fafed7e7c54e7a9c7f533ecc06f8231804fd31dd
PhilipOkwaro <96232766+PhilipOkwaro@users.noreply.github.com>
2022-03-04 16:30:12 +0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Zero Hunger</title>
+  <link rel="icon" type="image/x-icon" href="Favicon.jpg">
+  <style>
+body {font-family: Arial, Helvetica, sans-serif;}
+/*form {border: 3px solid #f1f1f1;}*/
+
+input[type=text], input[type=password], input[type=number], input[type=email] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+button:hover {
+  opacity: 0.8;
+}
+
+</style>
+</head>
+
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+        <!--<a href="#Hero-bn.jpeg">-->
+            <h1>Zero Hunger</h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#hero" data-after="Home">Home</a></li>
+            <li><a href="#services" data-after="Service">Goals</a></li>
+            <li><a href="#projects" data-after="Projects">Projects</a></li>
+            <li><a href="#about" data-after="About">About</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+            <!--<li><button class='loginbtn'onclick="document.getElementById('register-form').style.display='block'" style="width:auto;">Register</button></li>-->
+            <li><a href="#donations" data-after="donations">donations</a></li>
+
+            <!--<li><button class='loginbtn'onclick="document.getElementById('register-form').style.display='block'" style="width:auto;">Donations</button></li>-->
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+  <!-- Hero Section  -->
+  <section id="hero">
+   <div class="hero container">
+
+      <div>
+        <h1>Goal 2 <span></span></h1>
+        <h1>Zero <span></span></h1>
+        <h1>Hunger<span></span></h1>
+
+
+      </div>
+    </div>
+  </section>
+  <!-- End Hero Section  -->
+
+  <!-- Service Section -->
+  <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">Go<span>a</span>ls</h1>
+        <p>The Sustainable Development Goals or Global Goals are a collection of 17 interlinked global goals
+          designed to be a "blueprint to achieve a better and more sustainable future for all".</p>
+      </div>
+      <div class="service-bottom">
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/external-wanicon-lineal-color-wanicon/64/000000/external-nutrition-food-technology-wanicon-lineal-color-wanicon.png" /></div>
+          <h2>Malnutrition</h2>
+          <p>By 2030, end all forms of malnutrition, including achieving, by 2025, the internationally agreed targets on stunting and wasting in children under 5 years of age,
+            and address the nutritional needs of adolescent girls, pregnant and lactating women and older persons</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/64/000000/external-agriculture-agriculture-flatart-icons-lineal-color-flatarticons.png" /></div>
+          <h2>Agriculture</h2>
+          <p>By 2030, double the agricultural productivity and incomes of small-scale food producers, in particular women, indigenous peoples, family farmers,
+            pastoralists and fishers, including through secure and equal access to land, other productive resources and inputs</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/external-photo3ideastudio-lineal-color-photo3ideastudio/64/000000/external-smart-farming-internet-of-things-photo3ideastudio-lineal-color-photo3ideastudio.png" /></div>
+          <h2>Agricultural practices</h2>
+          <p>By 2030, ensure sustainable food production systems and implement resilient agricultural practices that increase productivity and production,
+            that help maintain ecosystems, that strengthen capacity for adaptation to climate change and extreme weather</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/fluency/48/000000/market-square.png" /></div>
+          <h2>Food Market</h2>
+          <p>Adopt measures to ensure the proper functioning of food commodity markets and their derivatives and facilitate timely access to market information,
+            including on food reserves, in order to help limit extreme food price volatility</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Service Section -->
+
+  <!-- Projects Section -->
+  <section id="projects">
+    <div class="projects container">
+      <div class="projects-header">
+        <h1 class="section-title">Recent <span>Projects</span></h1>
+      </div>
+      <div class="all-projects">
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 1</h1>
+            <h2>Sustainable Solutions to end hunger</h2>
+            <p>The ‘Sustainable Solutions to End Hunger’ research project, which focused on providing donor governments with new tools to help them increase poor producer’s incomes while simultaneously protecting the climate and ending hunger,
+              in line with SDG 2 (end hunger), has suggested undertaking interventions worth USD 14 billion on average each year.!</p>
+          </div>
+          <div class="project-img">
+            <img src="Hero-bn.jpeg" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 2</h1>
+            <h2>Food on the Move</h2>
+            <p>To reduce post-harvest losses, look beyond the storage of cereals to also focus on other food groups and other parts of the food value chain
+               Invest in the infrastructure, regulations, services and technical assistance needed to support small and medium-sized enterprises (SMEs) that supply or buy from small-scale farmers!</p>
+          </div>
+          <div class="project-img">
+            <img src="project2.jpg" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 3</h1>
+            <h2>Zero post-harvest losses</h2>
+            <p>WFP’s Zero Post-Harvest Losses project helps smallholder farmers learn how to use improved post-harvest handling methods,
+              combined with simple but effective hermetic storage equipment to protect crops against insects, rodents, mould and moisture.</p>
+          </div>
+          <div class="project-img">
+            <img src="project3.jpg" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 4</h1>
+            <h2>Food assistance</h2>
+            <p>While food aid is a tried and tested model, proudly woven into WFP history, it sprang from a largely unidirectional, top-down vision: people were hungry; we fed them. Food assistance, by contrast, involves a more complex understanding of people’s long-term nutritional needs and of the diverse approaches required to meet them. This conceptual shift has been at the core of WFP’s transformation in recent years.
+              While we remain the world’s leading humanitarian agency, we have evolved to combine frontline action with the quest for durable solutions.</p>
+          </div>
+          <div class="project-img">
+            <img src="project4.jpg" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 5</h1>
+            <h2>In-kind food distribution</h2>
+            <p>Providing food to people in need has long been the cornerstone of the World Food Programme (WFP)’s work, our historical raison d'être. While we have shifted our approach over the past decade – from food aid to food assistance in the form of cash transfers –
+              the provision of food in kind remains a fundamental part of our work and often the best solution in fragile or crisis situations.
+               This involves the distribution of a selection of foods chosen to prevent malnutrition and meet the energy needs of the people we serve. Food can be provided to everyone in a geographic area or a camp (known as blanket distribution) or provided to specific individuals or groups considered particularly vulnerable (targeted distribution).</p>
+          </div>
+          <div class="project-img">
+            <img src="project5.jpg" alt="img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Projects Section -->
+
+  <!-- About Section -->
+  <section id="about">
+    <div class="about container">
+      <div class="col-left">
+        <div class="about-img">
+          <img src="org.png" alt="img">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">Ab<span>ou</span>t</h1>
+        <h2>Charity Organizationr</h2>
+        <p>an organization whose primary objectives are philanthropy and social well-being. Focus majorly on satisfying the zero hunger goal
+        across every country and region mostly the vulnerable regions and people</p>
+        <!--<a href="#" class="cta">Download Resume</a>-->
+      </div>
+    </div>
+  </section>
+  <!-- End About Section -->
+
+  <!--donation section-->
+  <section id="donations">
+    <div class="donations container">
+      <div class="donations-top">
+        <h1 class="section-title">Do<span>na</span>tions</h1>
+        <form>
+          <div class="card-body">
+            <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+
+                              </div>
+                              <div class="form-group">
+                                <label for="exampleInputPassword1">Security</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Maximum 3 ">
+                              </div>
+                              <div class="form-group">
+                                <label for="exampleInputEmail1">Amount</label>
+                                <input type="number" class="form-control" id="exampleInputEmail1" placeholder="USD">
+
+                              </div>
+
+                              <div class="form-group">
+                                <label for="exampleInputEmail1">Card details</label>
+                                <input type="number" class="form-control" id="exampleInputEmail1" placeholder="Paypal">
+
+                              </div>
+                              <div class="form-group">
+                                <label for="exampleInputEmail1">Name</label>
+                                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter name">
+
+                              </div>
+
+
+                            </div>
+                            <!-- /.card-body -->
+
+                            <div class="card-footer">
+                              <button type="submit" class="btn btn-primary">Submit</button>
+
+
+
+      </div>
+    </form>
+    </div>
+  </section>
+  <!-- End Donations Section -->
+
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">Contact <span>info</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/phone.png" /></div>
+          <div class="contact-info">
+            <h1>Phone</h1>
+            <h2>+254707464831</h2>
+            <h2>02004567000</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png" /></div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2>info@gmail.com</h2>
+            <h2>zerohunger@gmail.com</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png" /></div>
+          <div class="contact-info">
+            <h1>Address</h1>
+            <h2>Nairobi, Kenya</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1><span>Zero</span>Hunger</h1>
+      </div>
+
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/twitter.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/behance.png" /></a>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  <!-- End Footer -->
+  <!--<script src="./app.js"></script>-->
+</body>
+
+</html>
